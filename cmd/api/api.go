52ee3e4d665aebe8c32d34ce40cb242e56319e61
Morpa <andremorpa@gmail.com>
2026-06-28 09:20:32 +0100

    feat(api): add comment endpoint

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -53,6 +53,8 @@ func (app *application) mount() http.Handler {
 				r.Get("/", app.getPostHandler)
 				r.Delete("/", app.deletePostHandler)
 				r.Patch("/", app.updatePostHandler)
+
+				r.Post("/comments", app.createCommentHandler)
 			})
 		})
 	})
